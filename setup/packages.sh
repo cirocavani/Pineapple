eacb0ed4f230e1f9b904e89ddf940c6f8a7c1048
Ciro Cavani <ciro.cavani@gmail.com>
2020-02-26 22:54:05 -0300

    Update Julia 1.3.1 -> 1.4.0-rc2 (cuDNN initialization has some issues in both julia version)

--- setup/packages.sh
+++ setup/packages.sh
@@ -3,5 +3,5 @@ DOWNLOAD_DIR=downloads
 CONDA_PKG=Miniconda3-4.7.12.1-Linux-x86_64.sh
 CONDA_URL=https://repo.anaconda.com/miniconda/$CONDA_PKG
 
-JULIA_PKG=julia-1.3.1-linux-x86_64.tar.gz
-JULIA_URL=https://julialang-s3.julialang.org/bin/linux/x64/1.3/$JULIA_PKG
+JULIA_PKG=julia-1.4.0-rc2-linux-x86_64.tar.gz
+JULIA_URL=https://julialang-s3.julialang.org/bin/linux/x64/1.4/$JULIA_PKG
