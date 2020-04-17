272ba60d1bd44c338880a87d0465aab5ffc79304
Ciro Cavani <ciro.cavani@gmail.com>
2020-04-16 22:21:28 -0300

    Julia 1.4.1

--- setup/packages.sh
+++ setup/packages.sh
@@ -3,5 +3,5 @@ DOWNLOAD_DIR=downloads
 CONDA_PKG=Miniconda3-py38_4.8.2-Linux-x86_64.sh
 CONDA_URL=https://repo.anaconda.com/miniconda/$CONDA_PKG
 
-JULIA_PKG=julia-1.4.0-linux-x86_64.tar.gz
+JULIA_PKG=julia-1.4.1-linux-x86_64.tar.gz
 JULIA_URL=https://julialang-s3.julialang.org/bin/linux/x64/1.4/$JULIA_PKG
